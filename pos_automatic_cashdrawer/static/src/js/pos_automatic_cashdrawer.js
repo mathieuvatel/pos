8ec03b245eaf7902ba2b4705b57e73164ddde8bc
Mathieu VATEL <mathieu@julius.fr>
2016-10-10 14:32:55 +0200

    [FIX] response of a valid amount given by the automatic cashdrawer

--- pos_automatic_cashdrawer/static/src/js/pos_automatic_cashdrawer.js
+++ pos_automatic_cashdrawer/static/src/js/pos_automatic_cashdrawer.js
@@ -47,7 +47,7 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
                     var answer_type = answer_info.match(answer_type_expression);
                     if (answer_type) {
                         // If there is an answer type
-                        if (answer_type[0] == "WR") {
+                        if (answer_type[0] == "WR" && answer_type[1] == "CANCEL") {
                             // Case #WR:CANCEL#b#c#d#e# : answer_type[0] == "WR" and answer_type[1] == "CANCEL"
                             // TODO : check what to do here. But I think this should do nothing.
                         }
@@ -55,13 +55,13 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
                             // Case #ER:xxxx#b#c#d#e# : answer_type[0] == "ER"
                             // TODO : check what to do here. But I think this wont append because the cash drawer wont give back this error.
                         }
-                        else if (answer_type[0] == "LEVEL") {
-                            // Case #0:LEVEL#b#c#d#e#: answer_type[0] == "LEVEL"
+                        else if (answer_type[0] == "WR" && answer_type[1] == "LEVEL") {
+                            // Case #WR:LEVEL#b#c#d#e#: answer_type[0] == "LEVEL"
                             // The return says that an amount was correctly given to the cache machine
                             var amount_expression = /[0-9]+/g;
                             var amount_expression = answer_info.match(amount_expression);
-                            var amount_in = amount_expression[1] / 100;
-                            var amount_out = amount_expression[2] / 100;
+                            var amount_in = amount_expression[0] / 100;
+                            var amount_out = amount_expression[1] / 100;
                             // TODO : Check the amount_out and what is display on screen ?
                             var amount_in = screen.format_currency_no_symbol(amount_in);
                             line.set_amount(amount_in);
@@ -112,21 +112,21 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
     });
 
     screens.PaymentScreenWidget.include({
-	    render_paymentlines : function(){
+            render_paymentlines : function(){
             this._super.apply(this, arguments);
-		    var self = this;
-		    // Directly call the automatic cashdrawer if the journal is checked
-//		    self.pos.proxy.automatic_cashdrawer_transaction_start($(this).data('cid'), self);
-		    this.$('.paymentlines-container').unbind('click').on('click','.automatic-cashdrawer-transaction-start', function(event){
+                    var self = this;
+                    // Directly call the automatic cashdrawer if the journal is checked
+//                  self.pos.proxy.automatic_cashdrawer_transaction_start($(this).data('cid'), self);
+                    this.$('.paymentlines-container').unbind('click').on('click','.automatic-cashdrawer-transaction-start', function(event){
             // Why this "on" thing links several time the button to the action if I don't use "unlink" to reset the button links before ?
-			//console.log(event.target);
-		        self.pos.proxy.automatic_cashdrawer_transaction_start($(this).data('cid'), self);
-		    });
-	    },
+                        //console.log(event.target);
+                        self.pos.proxy.automatic_cashdrawer_transaction_start($(this).data('cid'), self);
+                    });
+            },
 
-	    renderElement : function(){
-		    this._super.apply(this, arguments);
-		    var self = this;
+            renderElement : function(){
+                    this._super.apply(this, arguments);
+                    var self = this;
             this.$('.automatic-cashdrawer-connection-init').click(function(){
                 self.pos.proxy.automatic_cashdrawer_connection_init();
             });
@@ -136,7 +136,7 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
             this.$('.automatic-cashdrawer-display-backoffice').click(function(){
                 self.pos.proxy.automatic_cashdrawer_connection_display_backoffice();
             });
-	    },
+            },
 
     });
 });
