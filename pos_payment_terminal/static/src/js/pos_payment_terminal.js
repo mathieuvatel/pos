f2a998e687420751ee001f0f4b13a87be02aac3c
Mathieu VATEL <mathieu@julius.fr>
2016-10-11 18:23:01 +0200

    [IMP] launch automatically call to the payment terminal

--- pos_payment_terminal/static/src/js/pos_payment_terminal.js
+++ pos_payment_terminal/static/src/js/pos_payment_terminal.js
@@ -18,10 +18,51 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
     var _t = core._t;
     var QWeb = core.qweb;
 
-    models.load_fields("account.journal",['payment_mode']);
+    models.load_fields("account.journal", ['payment_mode']);
+
+    models.Paymentline = models.Paymentline.extend({
+        get_automatic_payment_terminal: function() {
+            if (this.cashregister.journal.payment_mode == 'card' && this.config.iface_payment_terminal) {
+                return true;
+            }
+        },
+    });
+
+//    devices.ProxyDevice.include({
+//        payment_terminal_transaction_start: function(line_cid, currency_iso){
+//            var line;
+//            var lines = this.pos.get_order().get_paymentlines();
+//            for ( var i = 0; i < lines.length; i++ ) {
+//                if (lines[i].cid === line_cid) {
+//                    line = lines[i];
+//                }
+//            }
+//
+//            var data = {'amount' : line.get_amount(),
+//                        'currency_iso' : currency_iso,
+//                        'payment_mode' : line.cashregister.journal.payment_mode};
+//            //console.log(JSON.stringify(data));
+//            this.message('payment_terminal_transaction_start', {'payment_info' : JSON.stringify(data)});
+//        },
+//    });
+//
+//
+//    screens.PaymentScreenWidget.include({
+//	    render_paymentlines : function(){
+//		this._super.apply(this, arguments);
+//		    var self  = this;
+//		    this.$('.paymentlines-container').unbind('click').on('click','.payment-terminal-transaction-start',function(event){
+//            // Why this "on" thing links severaltime the button to the action if I don't use "unlink" to reset the button links before ?
+//			//console.log(event.target);
+//			self.pos.proxy.payment_terminal_transaction_start($(this).data('cid'), self.pos.currency.name);
+//		    });
+//
+//	    },
+//
+//    });
 
     devices.ProxyDevice.include({
-        payment_terminal_transaction_start: function(line_cid, currency_iso){
+        payment_terminal_transaction_start: function(screen, currency_iso){
             var line;
             var lines = this.pos.get_order().get_paymentlines();
             for ( var i = 0; i < lines.length; i++ ) {
@@ -29,27 +70,24 @@ odoo.define('pos_payment_terminal.pos_payment_terminal', function (require) {
                     line = lines[i];
                 }
             }
-
-            var data = {'amount' : line.get_amount(),
-                        'currency_iso' : currency_iso,
-                        'payment_mode' : line.cashregister.journal.payment_mode};
+            var data = {
+                    'amount' : line.get_amount(),
+                    'currency_iso' : currency_iso,
+                    'payment_mode' : line.cashregister.journal.payment_mode};
             //console.log(JSON.stringify(data));
             this.message('payment_terminal_transaction_start', {'payment_info' : JSON.stringify(data)});
         },
     });
 
-
     screens.PaymentScreenWidget.include({
-	    render_paymentlines : function(){
-		this._super.apply(this, arguments);
-		    var self  = this;
-		    this.$('.paymentlines-container').unbind('click').on('click','.payment-terminal-transaction-start',function(event){
-            // Why this "on" thing links severaltime the button to the action if I don't use "unlink" to reset the button links before ?
-			//console.log(event.target);
-			self.pos.proxy.payment_terminal_transaction_start($(this).data('cid'), self.pos.currency.name);
-		    });
-
-	    },
-
+        click_paymentmethods: function(id) {
+            var self = this;
+            this._super.apply(this, arguments);
+            var line = this.pos.get_order().selected_paymentline;
+            var auto = line.get_automatic_payment_terminal();
+            if (auto) {
+                this.pos.proxy.payment_terminal_transaction_start(self, self.pos.currency.name);
+            }
+        },
     });
 });
